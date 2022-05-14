180ce22d37e4c7f806ddfb15c181e751a30b153f
Vinny <vinnyshipley@gmail.com>
2022-05-14 11:51:26 -0700

    organized into folders

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Header from './Header.js';
-import Footer from './Footer.js';
-import Main from './Main.js'
+import Header from '../Header/Header.js';
+import Footer from '../Footer/Footer.js';
+import Main from '../Main/Main.js'
 import Modal from 'react-bootstrap/Modal'
-import data from './Data.json'
-import './App.css'
+import data from '../Data.json'
+import '../App/App.css'
 
 
 
